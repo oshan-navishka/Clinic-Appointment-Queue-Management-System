0922a21ed46de0981da40a6ca9f62e4c6bf5063d
oshan-navishka <oshannavishka1234@gmail.com>
2026-08-20 14:20:25 +0530

    create clinic baranchers controller

---
+++ clinicAppointmentQueueManagementSystem/src/main/java/com/example/clinic_Appointment_Queue_Management_System/controller/ClinicBranchesController.java
@@ -0,0 +1,28 @@
+package com.example.clinic_Appointment_Queue_Management_System.controller;
+
+import com.example.clinic_Appointment_Queue_Management_System.dto.ClinicBranchesDTO;
+import com.example.clinic_Appointment_Queue_Management_System.dto.CommonResponse;
+import com.example.clinic_Appointment_Queue_Management_System.repository.ClinicBranchesRepository;
+import com.example.clinic_Appointment_Queue_Management_System.service.ClinicBranchesService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/clinicBranches")
+@RequiredArgsConstructor
+public class ClinicBranchesController {
+    private final ClinicBranchesService clinicBranchesService;
+
+    @PostMapping(value = "/addBranch", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse addClinicBranch(@RequestBody ClinicBranchesDTO clinicBranchesDTO) {
+        log.info("Adding clinic branch");
+        clinicBranchesService.saveClinicBranches(clinicBranchesDTO);
+        return new CommonResponse(0, "Clinic branch added successfully");
+    }
+}
