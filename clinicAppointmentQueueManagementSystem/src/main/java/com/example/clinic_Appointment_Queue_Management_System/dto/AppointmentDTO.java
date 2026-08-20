989709ab63f69ad5c96939b003e76942cb62735f
oshan-navishka <oshannavishka1234@gmail.com>
2026-08-20 16:17:04 +0530

    appoinment Dto

--- clinicAppointmentQueueManagementSystem/src/main/java/com/example/clinic_Appointment_Queue_Management_System/dto/AppointmentDTO.java
+++ clinicAppointmentQueueManagementSystem/src/main/java/com/example/clinic_Appointment_Queue_Management_System/dto/AppointmentDTO.java
@@ -1,4 +1,25 @@
 package com.example.clinic_Appointment_Queue_Management_System.dto;
 
+import com.example.clinic_Appointment_Queue_Management_System.enumaration.Status;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentDTO {
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private String branchId;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+    private int appointmentNumber;
+    private String reason;
+    private Status status;
 }
