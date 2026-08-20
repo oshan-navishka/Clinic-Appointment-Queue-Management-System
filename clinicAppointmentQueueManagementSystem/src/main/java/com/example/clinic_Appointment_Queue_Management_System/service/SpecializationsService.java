d11bf4539ef05da6e15292e6303c68f779e8a2e6
oshan-navishka <oshannavishka1234@gmail.com>
2026-08-20 13:51:04 +0530

    create Specializations Service classes

---
+++ clinicAppointmentQueueManagementSystem/src/main/java/com/example/clinic_Appointment_Queue_Management_System/service/SpecializationsService.java
@@ -0,0 +1,7 @@
+package com.example.clinic_Appointment_Queue_Management_System.service;
+
+import com.example.clinic_Appointment_Queue_Management_System.dto.SpecializationsDTO;
+
+public interface SpecializationsService {
+    void save_specialization(SpecializationsDTO specializationsDTO);
+}
---
+++ clinicAppointmentQueueManagementSystem/src/main/java/com/example/clinic_Appointment_Queue_Management_System/service/impl/SpecializationsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.clinic_Appointment_Queue_Management_System.service.impl;
+
+import com.example.clinic_Appointment_Queue_Management_System.dto.SpecializationsDTO;
+import com.example.clinic_Appointment_Queue_Management_System.entity.Specializations;
+import com.example.clinic_Appointment_Queue_Management_System.repository.SpecializationsRepository;
+import com.example.clinic_Appointment_Queue_Management_System.service.SpecializationsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SpecializationsServiceImpl implements SpecializationsService {
+    private final SpecializationsRepository specializationsRepository;
+    @Override
+    public void save_specialization(SpecializationsDTO specializationsDTO) {
+        log.info("In SpecializationsServiceImpl.save_specialization");
+        try {
+            long count = specializationsRepository.count();
+            String generatedId = String.format("S%03d", count + 1);
+
+            Specializations specializations = new Specializations();
+            specializations.setSpecializationId(generatedId);
+            specializations.setName(specializationsDTO.getName());
+            specializations.setDescription(specializationsDTO.getDescription());
+            specializations.setStatus(specializationsDTO.getStatus());
+            specializationsRepository.save(specializations);
+        }catch (Exception e){
+            log.error("Error saving specialization: {}", e.getMessage());
+            throw e;
+        }
+    }
+}
