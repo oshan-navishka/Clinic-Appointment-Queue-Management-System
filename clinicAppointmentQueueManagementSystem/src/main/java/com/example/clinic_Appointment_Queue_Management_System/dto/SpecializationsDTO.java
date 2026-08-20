7ab86fba1d062ff3ffdf35d6bee0c65f491ecc10
oshan-navishka <oshannavishka1234@gmail.com>
2026-08-20 13:42:37 +0530

    create Spesialize dto

--- clinicAppointmentQueueManagementSystem/src/main/java/com/example/clinic_Appointment_Queue_Management_System/dto/SpecializationsDTO.java
+++ clinicAppointmentQueueManagementSystem/src/main/java/com/example/clinic_Appointment_Queue_Management_System/dto/SpecializationsDTO.java
@@ -1,4 +1,15 @@
 package com.example.clinic_Appointment_Queue_Management_System.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SpecializationsDTO {
+    private String specializationId;
+    private String name;
+    private String description;
+    private String status;
 }
