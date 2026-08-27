6a1bd8a794418aa8bff2d5d8c1b8dc74f8cc5b93
oshan-navishka <oshannavishka1234@gmail.com>
2026-08-27 14:32:58 +0530

    update entity

--- clinicAppointmentQueueManagementSystem/src/main/java/com/example/clinic_Appointment_Queue_Management_System/repository/SpecializationsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.clinic_Appointment_Queue_Management_System.repository;
-
-import com.example.clinic_Appointment_Queue_Management_System.entity.Specializations;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpecializationsRepository extends JpaRepository<Specializations, String> {
-}
